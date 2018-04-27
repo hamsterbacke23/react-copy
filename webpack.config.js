4cf4c22519ddd5d9f470bc283cbfa1823b5d2164
Tobias Schmidt <tobias.schmidt82@gmail.com>
2018-04-27 10:07:48 +0200

    Fix travis

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: [
       {
         test: /\.js$/,
-        exclude: /(node_modules|build)/,
+        exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
           query: {
