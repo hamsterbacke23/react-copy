b691efafd38562bcb37744ae88be7c78f839c8b2
Tobias Schmidt <tobias.schmidt82@gmail.com>
2018-04-27 10:03:05 +0200

    Test 2

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -12,6 +14,33 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          query: {
+            plugins: ['transform-class-properties'],
+            presets: ['env', 'react']
+          }
+        }
+      }
+    ]
+  },
+  externals: {
+    react: 'commonjs react'
+  }
+};
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|build)/,
         use: {
           loader: 'babel-loader',
           query: {
